c74d618a16c643b8ac2ffd0509f8225f7dea73b6
Olga <klokova.plus@gmail.com>
2022-04-01 22:31:42 +0200

    fix

--- public/PageGarage.ts
+++ public/PageGarage.ts
@@ -160,7 +160,7 @@ export class PageGarage {
         // let arCars = arrsCars.then(res => started());
         // console.log( allCars, '!!!<<<<<<<');
         allCars.then(values => {
-            console.log(values = `http://localhost:3000/engine?id=${values.id}&status=started`);
+            // console.log(values = `http://localhost:3000/engine?id=${values.id}&status=started`);
             values.map((elem: { id: number; }) => {`http://localhost:3000/engine?id=${elem.id}&status=started`})
             Promise.all(values).then(values => {
                 console.log(values, '<<');
