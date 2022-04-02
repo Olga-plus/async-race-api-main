807ec9a2a9ee50da1693ac2795ffe1ecda04e63a
Olga <klokova.plus@gmail.com>
2022-04-02 09:36:35 +0200

    fix

--- public/PageGarage.ts
+++ public/PageGarage.ts
@@ -158,8 +158,10 @@ export class PageGarage {
             console.log(arr, 'NNn');
             let arrPromise = Promise.all(arr).then(values => {
                 values.map(elem => {
-                    fetch(elem).then(response => response.json().then(
-                        
+                    fetch(elem).then(response => response.json()
+                    .then(data => {
+                        console.log(data);
+                    }
                     ));
                 })
                 console.log(values, arrPromise, '>!<');
