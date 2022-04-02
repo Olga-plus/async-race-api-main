35644d271d33f7799488d26ae48d9cd1925af3e2
Olga <klokova.plus@gmail.com>
2022-04-02 12:30:21 +0200

    fix

--- public/PageGarage.ts
+++ public/PageGarage.ts
@@ -170,7 +170,7 @@ export class PageGarage {
                  {method: 'PATCH'}))
             }
            Promise.all(arr).then(res =>
-           this.car.startedAll({ res })
+           this.car.startedAll(res)
            ) 
     }
 }
