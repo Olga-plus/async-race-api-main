5a5767948bfa013a55c6e6329e8845d720894a76
Olga <klokova.plus@gmail.com>
2022-04-02 10:21:37 +0200

    fix

--- public/PageGarage.ts
+++ public/PageGarage.ts
@@ -179,6 +179,7 @@ export const garagePage = new PageGarage(callbackGarage);
                     .then(result => {
                         result;
                         body.innerHTML = '';
+                        this.createGaragePage();
                         cars(callbackCar);
                     });
                 break;
